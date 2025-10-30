5e52e03b6dd5a2e44eb0792a2e065274f3fecb91
Daniela Miranda Fernandez Cardoso <daniela.419@alunosatc.edu.br>
2025-10-30 19:32:18 -0300

    add

--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,52 @@
-console.log("Hello, World! 002");
+class Device {
+    turnOn() {}
+    turnOff() {}
+}
+  
+class TV extends Device {
+    turnOn() {
+      console.log("TV ligada.");
+    }
+    turnOff() {
+      console.log("TV desligada.");
+    }
+}
+  
+class Radio extends Device {
+    turnOn() {
+      console.log("Rádio ligado.");
+    }
+    turnOff() {
+      console.log("Rádio desligado.");
+    }
+}
+  
+class RemoteControl {
+    constructor(device) {
+      this.device = device;
+    }
+  
+    pressPowerButton(on) {
+      if (on) this.device.turnOn();
+      else this.device.turnOff();
+    }
+}
+  
+class AdvancedRemoteControl extends RemoteControl {
+    mute() {
+      console.log("Dispositivo mutado (silenciado).");
+    }
+}
+  
+const tv = new TV();
+const radio = new Radio();
+
+const tvRemote = new RemoteControl(tv);
+const radioRemote = new AdvancedRemoteControl(radio);
+
+tvRemote.pressPowerButton(true);   // TV ligada
+tvRemote.pressPowerButton(false);  // TV desligada
+
+radioRemote.pressPowerButton(true); // Rádio ligado
+radioRemote.mute();                 // Dispositivo mutado
+radioRemote.pressPowerButton(false); // Rádio desligado
